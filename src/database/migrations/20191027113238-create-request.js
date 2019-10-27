11b7e72b27b1bc527e59db5e2abf1b27f14ce7da
ObieWalker <obinnawalker@gmail.com>
2019-10-27 12:34:48 +0100

    add requests table

---
+++ src/database/migrations/20191027113238-create-request.js
@@ -0,0 +1,33 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Requests', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      requeterId: {
+        type: Sequelize.INTEGER
+      },
+      responded: {
+        type: Sequelize.BOOLEAN
+      },
+      donorId: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('Requests');
+  }
+};
