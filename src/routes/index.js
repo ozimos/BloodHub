0c7481a0961913c8d2f551938d51e21256ee5737
Olubodun Agbalaya <olubodun.agbalaya@andela.com>
2019-10-27 10:00:01 +0100

    local

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,5 +6,4 @@ const users = require('./users');
 router.use('/users', users);
 // router.use('/donors',require('./donors'));
 // router.use('/requester',require('./requester'));
-// router.use('/ibsbridge',require('./v1/IBSBridge'));
 module.exports = router;
