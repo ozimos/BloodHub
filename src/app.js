3681653c08cb7c657dfa87456795b92850398b94
Tovieye Moses Ozi <tovieye.ozi@gmail.com>
2019-10-28 08:49:15 +0100

    add cors middleware

--- src/app.js
+++ src/app.js
@@ -7,6 +7,11 @@ require('dotenv').config();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "https://bloodhub.herokuapp.com/"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
 
 const port = process.env.PORT;
 
