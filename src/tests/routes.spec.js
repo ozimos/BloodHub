90307c857646bca633b6abb73e840de31781e7a1
Tovieye Moses Ozi <tovieye.ozi@gmail.com>
2019-10-30 17:41:55 +0100

    add faker

--- src/tests/routes.spec.js
+++ src/tests/routes.spec.js
@@ -1,20 +1,25 @@
 import request from "supertest";
+import faker from "faker";
 import app from "../app";
 import db from "../models";
 
 const { User } = db;
+const bloodGroups = ["A+", "A-", "B+", "B-", "AB+", 'AB-', 'O+', 'O-'];
+const email = faker.internet.email();
+const password = faker.random.word();
+
 describe("Donor SignUp Endpoints", () => {
   const donorSignUpInput = {
-    firstName: "assad",
-    lastName: "hakeem",
-    phone: "0812345",
-    password: "asd",
-    verifyPassword: "asd",
-    street: "asd",
-    lg: "asd",
-    state: "adf",
-    email: "asd",
-    bloodGroup: "A+",
+    firstName: faker.name.firstName(),
+    lastName: faker.name.lastName(),
+    phone: faker.phone.phoneNumber(),
+    password,
+    verifyPassword: password,
+    street: faker.address.streetAddress(),
+    lg: faker.address.county(),
+    state: faker.address.state(),
+    email,
+    bloodGroup: faker.random.arrayElement(bloodGroups),
     document: "asd"
   };
 
@@ -29,6 +34,6 @@ describe("Donor SignUp Endpoints", () => {
     const { message, user } = res.body;
     const { firstName, lastName, email } = donorSignUpInput;
     expect(message).toBe("Your account has been created!, Your details");
-    expect(user).toStrictEqual({ firstName, lastName, email });
+    expect(user).toStrictEqual({ firstName, lastName, email: email.toLowerCase() });
   });
 });
